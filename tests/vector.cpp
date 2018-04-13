1599118bbef1717108d284d7a05ca9d528fb21b9
ArtemTszyu <32194510+ArtemTszyu@users.noreply.github.com>
2018-04-14 01:03:54 +0500

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -13,18 +13,21 @@ TEST_CASE("creating queue")
 TEST_CASE("copying queue")
 {
 	queue_t<int> queue;
-	queue.push(1);
-	queue.push(2);
-	queue_t<int> queue2(queue);
-	int sum = queue2.pop();
-	sum = sum + queue2.pop();
-	REQUIRE( sum == 3 );
+	queue_t<int> queue2;
+	queue.push(3);
+	queue.push(6);
 	
+	queue2.push(1);
+	
+	queue2 = queue;
+	
+	REQUIRE( queue2.headl(queue2.heado()) == 3 );
+	REQUIRE( queue2.taill(queue2.tailo()) == 6 );
 }
 
-TEST_CASE("pushing elements")
+TEST_CASE("pushing elements int")
 {
-	queue_t queue;
+	queue_t <int> queue;
 
 	queue.push(1);
 	REQUIRE( queue.head() == 1 );
@@ -39,9 +42,48 @@ TEST_CASE("pushing elements")
 	REQUIRE( queue.tail() == 3 );
 }
 
-TEST_CASE("poping elements")
+TEST_CASE("pushing elements double")
 {
-	queue_t queue;
+	queue_t <double> queue;
+
+	queue.push(1);
+	REQUIRE( queue.head() == 1 );
+	REQUIRE( queue.tail() == 1 );
+
+	queue.push(2);
+	REQUIRE( queue.head() == 1 );
+	REQUIRE( queue.tail() == 2 );
+
+	queue.push(3);
+	REQUIRE( queue.head() == 1 );
+	REQUIRE( queue.tail() == 3 );
+}
+
+TEST_CASE("poping elements int")
+{
+	queue_t <int> queue;
+
+	queue.push(1);
+	queue.push(2);
+	queue.push(3);
+	queue.push(4);
+
+	queue.pop();
+	REQUIRE( queue.head() == 2 );
+	REQUIRE( queue.tail() == 4 );
+
+	queue.pop();
+	REQUIRE( queue.head() == 3 );
+	REQUIRE( queue.tail() == 4 );
+
+	queue.pop();
+	REQUIRE( queue.head() == 4 );
+	REQUIRE( queue.tail() == 4 );
+}
+
+TEST_CASE("poping elements double")
+{
+	queue_t <double> queue;
 
 	queue.push(1);
 	queue.push(2);
